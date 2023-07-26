d199b544f729fac927a9b9adee06a9e59019dd8b
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-07-26 22:15:37 +0900

    Feat: 기본 리스트 만들기

--- src/components/TodoList.jsx/TodoList.jsx
+++ src/components/TodoList.jsx/TodoList.jsx
@@ -1,39 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TodoList() {
+  const [todos, setTodos] = useState(dafaultTodos);
+
   return (
     <div className='todoBox'>
       <div className='todoList'>
         <ul>
-          <li>
-            <div>
-              <input type='checkbox' name='' id='' />
-              <label htmlFor=''>
-                <span>공부</span>
-              </label>
-            </div>
-            <button>삭제</button>
-          </li>
-          <li>
-            <div>
-              <input type='checkbox' name='' id='' />
-              <label htmlFor=''>
-                <span>수영</span>
-              </label>
-            </div>
-            <button>삭제</button>
-          </li>
-          <li>
-            <div>
-              <input type='checkbox' name='' id='' />
-              <label htmlFor=''>
-                <span>카페가기</span>
-              </label>
-            </div>
-            <button>삭제</button>
-          </li>
+          {todos.map((todo) => {
+            const { id, title } = todo;
+            return (
+              <li key={id}>
+                <div>
+                  <input type='checkbox' name={`todo_${id}`} id={`todo_${id}`} />
+                  <label htmlFor={`todo_${id}`}>
+                    <span>{title}</span>
+                  </label>
+                </div>
+                <button>삭제</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
   );
 }
+
+const dafaultTodos = [
+  { id: '001', title: '공부', status: '진행중' },
+  { id: '002', title: '수영', status: '진행중' },
+  { id: '003', title: '카페가기', status: '진행중' },
+];
