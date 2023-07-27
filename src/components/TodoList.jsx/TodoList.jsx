e3ef5d6f4a0dd02130cbf6cc83f914ad53cf2f79
younu-Yun <dbssnfjd122@gmail.com>
2023-07-27 20:51:11 +0900

    Feat: todos localStorage에 저장

--- src/components/TodoList.jsx/TodoList.jsx
+++ src/components/TodoList.jsx/TodoList.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
 export default function TodoList({ selectedFilter }) {
-  const [todos, setTodos] = useState(dafaultTodos);
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
 
-  //투두 추가
+  //todos 추가
   const addTodo = (addedTodo) => setTodos([...todos, addedTodo]);
-  //투두 삭제
+  //todos 삭제
   const deleteTodo = (deletedTodo) =>
     setTodos(todos.filter((todo) => todo.id !== deletedTodo.id));
-  //투두 상태변경
+  //todos 상태변경
   const updateTodo = (updatedTodo) =>
     setTodos(
       todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
     );
-  //투두 수정
+  //todos 수정
   const changeTodo = (changedTodo) => {
     console.log(changedTodo);
     setTodos(
@@ -23,7 +23,13 @@ export default function TodoList({ selectedFilter }) {
     );
   };
 
+  //filter된 todos
   const selectedTodos = getFilteredTodos(todos, selectedFilter);
+
+  //todo가 변경될때마다
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="todoBox">
@@ -46,15 +52,16 @@ export default function TodoList({ selectedFilter }) {
   );
 }
 
-const dafaultTodos = [
-  { id: '001', title: '공부', status: '진행중' },
-  { id: '002', title: '수영', status: '진행중' },
-  { id: '003', title: '카페가기', status: '진행중' },
-];
-
 function getFilteredTodos(todos, selectedFilter) {
   if (selectedFilter === '전체') {
     return todos;
   }
   return todos.filter((todo) => todo.status === selectedFilter);
+}
+
+function readTodosFromLocalStorage() {
+  console.log('chek');
+  const todos = localStorage.getItem('todos');
+  //todos가 localStorage에 있으면 불러오고, 아니면 빈배열 return
+  return todos ? JSON.parse(todos) : [];
 }
